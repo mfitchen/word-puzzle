4d240beecc9641d720ce7edd7139994521eb426e
Marty Fitchen and Noah Yasskin <marty@>
2016-06-07 10:03:10 -0700

    add JavaScript and HTML elements

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,27 @@
 
 $(document).ready(function() {
 
-    event.preventDefault();
+//var vowelsArray = ["a", "e", "i", "o", "u"]
+  var puzzleString = "You miss 100% of the shots you don't take. Wayne Gretzky";
+  var newPuzzleArray = puzzleString.split();
+
+  for (index = 0; index < newPuzzleArray.length; index += 1) {
+    if (newPuzzleArray[index] === "a" || newPuzzleArray[index] === "e" || newPuzzleArray[index] === "i" || newPuzzleArray[index] === "o" || newPuzzleArray[index] === "u") {
+      newPuzzleArray[index] === "-"
+    }
+  };
+
+  newPuzzleArray.join();
+
+  console.log(newPuzzleArray);
+
+  $("#puzzle-guess").show(newPuzzleArray);
+// alert(newPuzzleString);
+// display#puzzle-guess
+//
+// display#puzzle-output
+
+  // event.preventDefault();
 });
+
+//newPuzzleString = puzzleString.replace(vowelsArray[index], "-");
